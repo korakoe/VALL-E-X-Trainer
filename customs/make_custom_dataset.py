a947e628259df155ba433668a3d0fc66983b7aec
ButterCream <56580073+korakoe@users.noreply.github.com>
2023-11-04 20:44:26 +0800

    LJSpeech Dataset Support

--- customs/make_custom_dataset.py
+++ customs/make_custom_dataset.py
@@ -23,8 +23,8 @@ tokenizer_path = "./utils/g2p/bpe_69.json"
 tokenizer = PhonemeBpeTokenizer(tokenizer_path)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def make_prompts(name, audio_prompt_path, transcript=None):
-    text_tokenizer = PhonemeBpeTokenizer(tokenizer_path="./utils/g2p/bpe_69.json")
+def make_prompts(name, audio_prompt_path, transcript=None, cleaner="cje_cleaners"):
+    text_tokenizer = PhonemeBpeTokenizer(tokenizer_path="./utils/g2p/bpe_69.json", cleaner=cleaner)
     text_collater = get_text_token_collater()
     codec = AudioTokenizer(device)
     wav_pr, sr = torchaudio.load(audio_prompt_path)
@@ -35,12 +35,15 @@ def make_prompts(name, audio_prompt_path, transcript=None):
         wav_pr = wav_pr.mean(0, keepdim=True)
     text_pr, lang_pr = make_transcript(name, wav_pr, sr, transcript)
 
+    if transcript:
+      text_pr = transcript
+
     # tokenize audio
     encoded_frames = tokenize_audio(codec, (wav_pr, sr))
     audio_tokens = encoded_frames[0][0].transpose(2, 1).cpu().numpy()
 
     # tokenize text
-    phonemes, langs = text_tokenizer.tokenize(text=f"{text_pr}".strip())
+    phonemes, langs = text_tokenizer.tokenize(text_pr)
     text_tokens, enroll_x_lens = text_collater(
         [
             phonemes
@@ -79,5 +82,42 @@ def create_dataset(data_dir, dataloader_process_only):
                     except Exception as e:
                         print(f"An error occurred: {e}")
     else:
-        dataloader = create_dataloader(data_dir=data_dir, max_duration=20)
+        dataloader = create_dataloader(data_dir=data_dir)
+        return dataloader
+
+def create_dataset_ljspeech(dir_to_txt, dataloader_process_only):
+    if dataloader_process_only:
+        h5_output_path=f"{os.path.dirname(dir_to_txt)}/audio_sum.hdf5"
+        ann_output_path=f"{os.path.dirname(dir_to_txt)}/audio_ann_sum.txt"
+        #audio_folder = os.path.join(data_dir, 'audio')
+        with open(dir_to_txt, "r", encoding="utf8") as file:
+            wavs_txt = file.readlines()
+
+        # Create or open an HDF5 file
+        with h5py.File(h5_output_path, 'w') as h5_file:
+            # Loop through each audio and text file, assuming they have the same stem
+            for speech in wavs_txt:
+                speech = speech.replace("\n", "").split("|")
+                print(speech)
+                audio_path = os.path.join(os.path.dirname(dir_to_txt), speech[0])
+                stem = os.path.splitext(os.path.basename(audio_path))[0]
+                audio_tokens, text_tokens, langs, text = make_prompts(name=stem, audio_prompt_path=audio_path, transcript=speech[1], cleaner="cje_cleaners")
+                
+                text_tokens = text_tokens.squeeze(0)
+                # Create a group for each stem
+                grp = h5_file.create_group(stem)
+                # Add audio and text tokens as datasets to the group
+                grp.create_dataset('audio', data=audio_tokens)
+                #grp.create_dataset('text', data=text_tokens)
+                
+                with open(ann_output_path, 'a', encoding='utf-8') as ann_file:
+                    try:
+                        audio, sample_rate = sf.read(audio_path)
+                        duration = len(audio) / sample_rate
+                        ann_file.write(f'{stem}|{duration}|{langs[0]}|{text}\n')  # 改行を追加
+                        print(f"Successfully wrote to {ann_output_path}")
+                    except Exception as e:
+                        print(f"An error occurred: {e}")
+    else:
+        dataloader = create_dataloader(data_dir=os.path.dirname(dir_to_txt))
         return dataloader
